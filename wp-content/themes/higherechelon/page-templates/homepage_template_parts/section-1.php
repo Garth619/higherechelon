f7128e422553560eca9cd3982eac9aad12e44364
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-17 10:42:08 -0800

    push

--- wp-content/themes/higherechelon/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/higherechelon/page-templates/homepage_template_parts/section-1.php
@@ -16,13 +16,13 @@
 			
 			<div class="sec_one_col one">
 				
-				<div class="sec_one_col_inner one">
+				<div class="sec_one_col_inner_intro">
 					
 					<img src="<?php bloginfo('template_directory');?>/images/he-logo-badge.svg"/>
 					
 					<span class="sec_one_col_content">HigherEchelon, Inc. (HE) is<br/> the premiere trusted partner in organizational excellence.</span><!-- sec_one_col_content -->
 					
-				</div><!-- sec_one_col_inner -->
+				</div><!-- sec_one_col_inner_intro -->
 				
 			</div><!-- sec_one_col -->
 			
@@ -40,9 +40,36 @@
 				
 			</div><!-- sec_one_col -->
 			
-			<div class="sec_one_col three"></div><!-- sec_one_col -->
 			
-			<div class="sec_one_col four"></div><!-- sec_one_col -->
+			<div class="sec_one_col three">
+				
+				<div class="sec_one_col_inner">
+					
+					<span class="sec_one_col_title">Digital<br/> Transformation</span><!-- sec_one_col_title -->
+					
+					<span class="sec_one_col_description">Lorem ipsum dolor sit amet consectetur adipiscing elit.</span><!-- sec_one_col_description -->
+					
+					<a class="sec_one_learn_more" href="">Learn More</a><!-- sec_one_learn_more -->
+					
+				</div><!-- sec_one_col_inner -->
+				
+			</div><!-- sec_one_col -->
+			
+			
+			<div class="sec_one_col four">
+				
+				<div class="sec_one_col_inner">
+					
+					<span class="sec_one_col_title">Change<br/> Management</span><!-- sec_one_col_title -->
+					
+					<span class="sec_one_col_description">Lorem ipsum dolor sit amet dolor sitconsectetur adipiscing elit.</span><!-- sec_one_col_description -->
+					
+					<a class="sec_one_learn_more" href="">Learn More</a><!-- sec_one_learn_more -->
+					
+				</div><!-- sec_one_col_inner -->
+				
+			</div><!-- sec_one_col -->
+			
 						
 		</div><!-- sec_one_bottom -->
 
