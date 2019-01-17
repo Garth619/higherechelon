1e1957fdd0cd7e8b64ab1cd581851aa0657f2aa7
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-17 10:31:30 -0800

    push

--- wp-content/themes/higherechelon/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/higherechelon/page-templates/homepage_template_parts/section-1.php
@@ -8,10 +8,43 @@
 		
 		<div class="sec_one_inner">
 			
-			<span class="sec_one_title">Helping companies take organizational performance to a higher level.</span><!-- sec_one_title -->
+			<span class="sec_one_title">Helping companies take <strong>organizational performance</strong> to a higher level.</span><!-- sec_one_title -->
 			
 		</div><!-- sec_one_inner -->
-
+		
+		<div class="sec_one_bottom">
+			
+			<div class="sec_one_col one">
+				
+				<div class="sec_one_col_inner one">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/he-logo-badge.svg"/>
+					
+					<span class="sec_one_col_content">HigherEchelon, Inc. (HE) is<br/> the premiere trusted partner in organizational excellence.</span><!-- sec_one_col_content -->
+					
+				</div><!-- sec_one_col_inner -->
+				
+			</div><!-- sec_one_col -->
+			
+			<div class="sec_one_col two">
+				
+				<div class="sec_one_col_inner">
+					
+					<span class="sec_one_col_title">Transformational<br/> Leadership</span><!-- sec_one_col_title -->
+					
+					<span class="sec_one_col_description">Lorem ipsum dolor sit amet consectetur adipiscing elit.</span><!-- sec_one_col_description -->
+					
+					<a class="sec_one_learn_more" href="">Learn More</a><!-- sec_one_learn_more -->
+					
+				</div><!-- sec_one_col_inner -->
+				
+			</div><!-- sec_one_col -->
+			
+			<div class="sec_one_col three"></div><!-- sec_one_col -->
+			
+			<div class="sec_one_col four"></div><!-- sec_one_col -->
+						
+		</div><!-- sec_one_bottom -->
 
 </section><!-- section_one -->
 
