c67bce91a7fef2c4f6c398888505a354965231cd
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-24 15:20:18 -0800

    push

--- wp-content/themes/higherechelon/page-templates/template-padirectory.php
+++ wp-content/themes/higherechelon/page-templates/template-padirectory.php
@@ -5,8 +5,21 @@
 get_header(); ?>
 
 
+<div id="internal_main">
+	
+	<div class="outer_container">
+	
+		<div class="pa_directory_wrapper">
+			
+			<h1 class="internal_header header_border pa_direct_header"><?php the_title();?></h1>
+			
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+			
+		</div><!-- pa_directory_wrapper -->
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+	</div><!-- outer_container -->
+	
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
--- wp-content/themes/higherechelon/page.php
+++ wp-content/themes/higherechelon/page.php
@@ -23,23 +23,9 @@
 		
 		<div class="inner_container content">
 			
-			<h1>Header Styling 1 - lorem ipsum dolor enusit</h1>
+			<h1><?php the_title();?></h1>
 			
-			<h2>H2 Styling  - Innovative capabilities to serve the needs of america</h2>
-			
-			<p>HigherEchelon, Inc. (HE) is a consulting firm providing training, technology and engineering services that integrates people, processes and technology to optimize performance. As such, we work at the intersection of leadership and technology to help organizations overcome their challenges and drive organizational excellence!</p>
-
-			<h3>H3 - OUR SERVICES</h3>
-			
-			<p>HigherEchelon (HE) delivers Training Technology, Salesforce Implementation, Enterprise Technology, and Engineering Services while working with clients to identify requirements, develop strategies to fulfill their needs, and deliver on those needs to achieve the clients’ goals. <a href="">embedded link</a> HigherEchelon invests in people, processes, and technology to drive organizational performance to a higher level. Founded on performance psychology and coupled with human capital, and technological service offerings, we work with organizations to address the most complex and ambiguous challenges. HigherEchelon’s competitive edge resides in our ability to bring these three domains together to maximize human and organizational excellence.</p>
-			
-			<ul>
-				
-				<li>To develop high performing organizations, we focus on People, Processes, and Bottom-Line Benefits.</li>
-				<li>Our people are industry experts who work alongside clients to create winning strategies that support improvements in effectiveness, efficiency, and productivity. </li>
-				<li>Our ideal vision includes high performing teams, using efficient processes, in a culture that supports and drives the business strategy.</li>
-				
-			<p>Our focus on technology services provides the tools that our clients use to optimize organizational performance. HigherEchelon has a highly skilled and experienced team of IT and Engineering professionals. From development of missile systems to gaming software to Salesforce CRM implementations, we develop solutions that impact organizational performance.</p>
+			<?php get_template_part( 'loop', 'page' ); ?>
 			
 		</div><!-- inner_container -->
 		
@@ -50,9 +36,5 @@
 	
 </div><!-- internal_main -->
 		
-	<?php // get_template_part( 'loop', 'page' ); ?>
-		
-
-
-
+	
 <?php get_footer(); ?>
