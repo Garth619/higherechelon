aee99e87616392bd48bb9cead38cc521066db0c7
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-24 13:27:20 -0800

    push

--- wp-content/themes/higherechelon/sidebar.php
+++ wp-content/themes/higherechelon/sidebar.php
@@ -1,12 +1,14 @@
 <div class="sidebar_wrapper">
 	
-	
-	
-	
+	<div class="sidebar">
+		
+		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		
+		
+	</div><!-- sidebar -->
 	
 </div><!-- sidebar_wrapper -->
 
 
 
 
-	<?php // wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
