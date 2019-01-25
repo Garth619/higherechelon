5f67e94b005433b8e9c0506d85fe389d32777ba2
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-25 09:42:33 -0800

    push

--- wp-content/themes/higherechelon/page-templates/template-bio.php
+++ wp-content/themes/higherechelon/page-templates/template-bio.php
@@ -15,7 +15,13 @@ get_header(); ?>
 			
 				<div class="att_image">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/att-caslen.jpg"/>
+					<?php $attorney_image = get_field( 'attorney_image' ); ?>
+					
+					<?php if ( $attorney_image ) { ?>
+					
+						<img src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>" />
+					
+					<?php } ?>
 				
 					</div><!-- att_image -->
 					
@@ -25,27 +31,35 @@ get_header(); ?>
 				
 							<div class="att_meta">
 					
-								<span class="att_position">Advisor</span><!-- att_position -->
+								<span class="att_position"><?php the_field( 'attorney_position' ); ?></span><!-- att_position -->
 						
 								<div class="att_social">
+									
+									<?php if(get_field('linkedin')): ?>
 							
-									<a class="linkedin" href="">
+										<a class="linkedin" href="<?php the_field( 'linkedin' ); ?>" target="_blank" rel="noopener">
 								
-										<?php echo file_get_contents("wp-content/themes/higherechelon/images/linkedin.svg"); ?>
+											<?php echo file_get_contents("wp-content/themes/higherechelon/images/linkedin.svg"); ?>
 								
-									</a>
+										</a>
+									
+									<?php endif;?>
+									
+									<?php if(get_field('email')): ?>
 							
-									<a class="email" href="">
+										<a class="email" href="mailto:<?php the_field( 'email' ); ?>" rel="noopener">
 								
-										<?php echo file_get_contents("wp-content/themes/higherechelon/images/email.svg"); ?>
+											<?php echo file_get_contents("wp-content/themes/higherechelon/images/email.svg"); ?>
 								
-									</a>
+										</a>
+									
+									<?php endif;?>
 							
 								</div><!-- att_social -->
 					
 							</div><!-- att_meta -->
 							
-							<span class="att_intro">Lt. Gen. Robert L. Caslen is a United States veteran who served in the U.S. Army for 43 years. His military career culminated in 2018 as the 59th superintendent of the United States Military Academy at West Point.</span><!-- att_intro -->
+							<span class="att_intro"><?php the_field( 'attorney_intro' ); ?></span><!-- att_intro -->
 				
 					</div><!-- att_title_wrapper -->
 				
@@ -53,20 +67,9 @@ get_header(); ?>
 				
 				<div class="att_bio_bottom content">
 					
-					<span class="att_content_header">LT. GeNERAL (RETIRED), US ARMY</span><!-- att_content_header -->
+					<span class="att_content_header"><?php the_field( 'attorney_bottom_intro_header' ); ?></span><!-- att_content_header -->
 					
-					<p>Under his direction as Superintendent, the Academy was recognized as the number one public college in the Nation by Forbes Magazine and the number one public college by U.S. News and World Report, and earned over 150 competitive scholarships (Rhodes, Draper, Marshall, Fulbright, East-West, Lincoln Labs, among others). Through the establishment of Centers of Excellence at West Point, General Caslen developed an integration between Army applied problem sets and West Point research and intellectual capital. Caslen refined West Point’s leadership program with a focus on professional ethics as an essential part of leadership and character development. He drove an update of the academic curriculum to embrace 21st century technology and information demands. He stood up the Army Cyber Institute to build skills and Army leaders required for the new cyber fight. He won support of Army leadership and of Congress to build a new $185 million barracks and a new $165 million Cyber Engineering Center, won an $850 million authorization to refurbish existing barracks over nine years, and won a $950 million authorization to refurbish academic buildings over ten. He worked extensively with Congress, the NCAA Division 1 Board of Governors, and West Point’s staff, faculty, and cadets to prevent sexual assault and eliminate sexual violence. He worked tirelessly to expand diversity for cadets and faculty alike, and dramatically expanded West Point’s minority and women populations to reflect the demographics of the Army that West Point’s graduates help lead. Working with Director of Athletics, he revamped the athletic program, made it self-sustaining, and fielded 25 competitive intercollegiate teams. He created partnerships with the NCAA and Department of Defense to conduct research into preventing and treating concussions and traumatic brain injury that have led to significant improvements in prevention and care. And, fulfilling a pledge he made soon after becoming Superintendent, he led a sweeping reversal of Army’s football program, culminating in Army defeating Navy in 2016 for the first time in 14 years, and beating both Air Force and Navy in 2017 to win the Commander-in-Chief’s trophy for the first time in 21 years.</p>
-					<p>Before becoming the West Point Superintendent, Lt. Gen. Caslen served as the chief of the Office of Security Cooperation in Iraq, where he served as the senior military commander in Iraq after the drawdown of U.S. and allied forces in 2011. Lt. Gen. Caslen also served as the commandant of the United States Army Command and General Staff College and the commanding general of the U.S. Army Combined Arms Center at Fort Leavenworth.</p>
-					
-					<p>During his time in the U.S. Army, Lt. Gen Caslen held several other notable positions including commanding general of the 25th Infantry Division, commanding general of the Multi-National Division- North during Operation Iraqi Freedom and Commandant of Cadets for the U.S. Military Academy. He served as Chief of Staff of both the 101st Airborne Division (AASLT) and the 10th Mountain Division, where he also served as Chief of Staff of TF Mountain during Operation Enduring Freedom.</p>
-					
-					<p>He also has served in combat and overseas deployments in Operation Desert Shield/Desert Storm, JTF-B in Honduras, and Operation Uphold Democracy and the United Nations Mission in Haiti.</p>
-					
-					<p>A graduate of the U.S. Military Academy at West Point, Lt. Gen Caslen holds a Master of Business Administration degree from Long Island University and a Master of Science degree in Industrial Engineering from Kansas State University. His awards and decorations include the Defense Distinguished Medal, the Distinguished Service Medal with two oak leaf clusters, the Defense Superior Service Medal with oak leaf cluster, the Legion of Merit with four oak leaf clusters, the Bronze Star Medal with two oak leaf clusters, the Defense Meritorious Service Medal and the Meritorious Service Medal with five oak leaf clusters. He also earned the Combat Infantryman Badge, the Joint Chiefs of Staff Identification Badge and is Airborne, Air Assault and Ranger qualified.</p>
-					
-					<p>He is a member of the Kansas State Engineering College Hall of Fame, recognized for a Life Time of Service Award from the American Red Cross, was selected as the Honorary Rock of the Year Award for his efforts in diversity as the Superintendent of West Point, and was recognized with an honorary Doctorate from Long Island University.</p>
-					
-					<p>He is married to the former Michele Pastin, from Highland Falls, N.Y. for 41 years and has 3 sons and 4 grandchildren.</p>
+					<?php get_template_part( 'loop', 'page' ); ?>
 					
 				</div><!-- att_bio_bottom -->
 		
