8c700cf826f52350ea066e6e058f0cd23cdd9fff
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-30 09:05:19 -0800

    push

--- wp-content/themes/higherechelon/page-templates/template-about.php
+++ wp-content/themes/higherechelon/page-templates/template-about.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		
 		<div class="banner_inner">
 			
-			<span class="banner_header about_header">Human Capital Services</span><!-- banner_header -->
+			<span class="banner_header about_header"><?php the_title();?></span><!-- banner_header -->
 			
 		</div><!-- banner_inner -->
 		
@@ -26,15 +26,13 @@ get_header(); ?>
 			
 			<div class="about_page_left">
 				
-				<h1>Excellence in Human Capital Services</h1>
+				<h1><?php the_field( 'sub_homepage_header' ); ?></h1>
 				
 			</div><!-- about_page_left -->
 			
 			<div class="about_page_right content">
 				
-				<h2>Our Human Capital Services (HCS) services offerings focus on the “people and process” part of our mission.</h2>
-				
-				<p>Our Human Capital Services (HCS) services offerings focus on the “people and process” part of our mission. Any organization’s journey to high performance starts with its people, so we believe it takes high performing leaders to drive organizational excellence. While many traditional leadership programs focus on the technical and tactical skills of leadership—"how to lead," we focus on the skills necessary for leaders to perform at a higher level, so they can go from "good to great." We have found that combining mental and emotional skills from the field of high performance psychology used by Olympians, surgeons, professional athletes, and other high performers (to include businessmen/women) unlocks a leader’s full ability and all allows him or her to maximize the effectiveness of the traditional leadership skills. In essence, we treat leadership like a performance, and we instill this practice into all service offerings to ensure you achieve a culture of high performance.</p>
+				<?php the_field( 'sub_homepage_content' ); ?>
 				
 			</div><!-- about_page_right -->
 			
@@ -46,7 +44,13 @@ get_header(); ?>
 				
 				<div class="about_middle_image">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/home_human_capital_services.jpg"/>
+					<?php $sub_homepage_about_middle_image = get_field( 'sub_homepage_about_middle_image' ); ?>
+					
+					<?php if ( $sub_homepage_about_middle_image ) { ?>
+					
+					<img src="<?php echo $sub_homepage_about_middle_image['url']; ?>" alt="<?php echo $sub_homepage_about_middle_image['alt']; ?>" />
+					
+					<?php } ?>
 					
 				</div><!-- about_middle_image -->
 				
@@ -54,7 +58,7 @@ get_header(); ?>
 					
 					<div class="about_middle_title_wrapper">
 						
-						<span class="about_middle_title">Our Services</span><!-- about_middle_title -->
+						<span class="about_middle_title"><?php the_field( 'sub_homepage_middle_title' ); ?></span><!-- about_middle_title -->
 						
 						<span class="about_middle_line"></span><!-- about_middle_line -->
 						
@@ -62,18 +66,19 @@ get_header(); ?>
 					
 					<div class="about_middle_lists">
 						
-						<ul>
-							<li>The Resilient + Adaptable Leader&copy; Program</li>
-							<li>Executive Coaching</li>
-							<li>Assessments</li>
-							<li>Applied High Performance</li>
-							<li>Team Building</li>
-							<li>Training + Education</li>
-							<li>Culture + Strategy Alignment</li>
-							<li>Strategic Planning</li>
-							<li>Process Improvement</li>
-							<li>Talent Management</li>
-						</ul>
+						<?php if(get_field('sub_homepage_middle_list_items')): ?>
+							
+							<ul>
+						 
+							<?php while(has_sub_field('sub_homepage_middle_list_items')): ?>
+						 
+								<li><?php the_sub_field( 'list_item' ); ?></li>
+						    
+							<?php endwhile; ?>
+							
+							</ul>
+						 
+						<?php endif; ?>
 						
 					</div><!-- about_middle_lists -->
 					
@@ -89,13 +94,7 @@ get_header(); ?>
 			
 			<div class="about_bottom_inner content">
 				
-				<h2>Human Performance is a mindset forged from experiences, education, and a strong sense of purpose</h2>
-				
-				<p>HigherEchelon’s approach to leader development is grounded in principles from the science of high performance and organizational psychology, and has been solidified through our experiences working with experts in business, military sport, and surgery. HigherEchelon’s mission is to enhance the self-awareness, agility, and adaptability of leaders through customized programs designed to meet your unique challenges.</p>
-
-				<p>Two decades of research has shown resiliency in an organization starts with the leader’s ability to deliberately and consistently apply cognitive and emotional skills to enhance performance. Resilient individuals can cultivate positive emotions in themselves and have the skills to elicit positive emotions in others because of their ability to effectively communicate, solve complex problems, and the confidence to lead in challenging environments. Resilient leaders align their own values and vision with that of the organization, while inspiring those around them to thrive to push the organization forward.</p>
-
-				<p>Human performance is regulated through cognitive, emotional, motivational, and decisional processes, especially during setbacks and challenging circumstances, thus leaders should invest in the development of adaptive mindsets, goal achievement, energy management, situational awareness, and self-awareness to improve performance. HigherEchelon understands the systematic approach to building resilient and adaptable leaders.</p>
+				<?php the_field( 'sub_homepage_bottom_content' ); ?>
 				
 			</div><!-- about_bottom_inner -->
 			
